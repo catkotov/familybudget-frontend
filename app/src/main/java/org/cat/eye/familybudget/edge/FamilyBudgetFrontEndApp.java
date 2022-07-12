8dc44ce4a4c877b362e9cdf42e80e21cb49367e3
Котов Александр <aleksandr.a.kotov@gazprombank.ru>
2022-07-12 18:28:15 +0300

    spring boot plugin was added

---
+++ app/src/main/java/org/cat/eye/familybudget/edge/FamilyBudgetFrontEndApp.java
@@ -0,0 +1,13 @@
+package org.cat.eye.familybudget.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBudgetFrontEndApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyBudgetFrontEndApp.class, args);
+    }
+
+}
